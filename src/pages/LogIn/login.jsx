4eebccc3ebd943bcce096eceb187fd4b81f139b6
aldoroman27 <148840055+aldoroman27@users.noreply.github.com>
2024-04-08 14:07:44 -0600

    Update login.jsx

--- src/pages/LogIn/login.jsx
+++ src/pages/LogIn/login.jsx
@@ -4,48 +4,68 @@ import user_icon from '../../assets/person.png'
 import email_icon from '../../assets/email.png'
 import password_icon from '../../assets/password.png'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
-    const [action, setAction] = useState("Iniciar Sesión");
+    const [action, setAction] = useState('Iniciar Sesión');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState('');
     const [loginSuccess, setLoginSuccess] = useState(false);
+    const navigate = useNavigate();
 
-
-    function handleSubmit(event) {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        axios.post('http://localhost:3000/Login', {email, password})
-        .then(res => {
+        try {
+            const res = await axios.post('http://localhost:3000/Login', { email, password });
             console.log(res);
-            setLoginSuccess(true);
-            setEmail('');
-            setPassword('');
-        })
-        .catch(err => console.log(err));
-    }
-    
-  return (
-    <div className='container'>
-        <div className='header'>
-            <div className='text'>{action}</div>
-            <div className='underline'></div>
-        </div>
-        <form onSubmit={handleSubmit}>
-            <div className='inputs'>
-                <div className='input'>
-                    <img src={email_icon} alt=""/>
-                    <input type="email" placeholder='Correo Electrónico' onChange={e => setEmail(e.target.value)}/>
-                </div>
-                <div className='input'>
-                    <img src={password_icon} alt=""/>
-                    <input type="password" placeholder='Contraseña' onChange={e => setPassword(e.target.value)}/>
-                </div>
+            if (res.data === 'Login Success') {
+                console.log('Inicio de sesión exitoso');
+                setLoginSuccess(true);
+                setLoginError('');
+            } else {
+                console.log('Credenciales incorrectas');
+                setLoginSuccess(false);
+                setLoginError('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+            }
+        } catch (error) {
+            console.error(error);
+            setLoginSuccess(false);
+            setLoginError('Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.');
+        }
+    };
+
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>{action}</div>
+                <div className='underline'></div>
             </div>
-           <div className='forgot-password'>¿Olvidaste tu contraseña?<span>Haz click aqui</span></div>
-           <br></br>
-           <button className='bttn-enviar' type="submit" onClick={handleSubmit}>Verificar información</button>
-           {loginSuccess && <div className='success-message'>¡Inicio de sesión exitoso, Bienvenido!</div>}
-        </form>
-    </div>
-  )
-}
+            <form onSubmit={handleSubmit}>
+                <div className='inputs'>
+                    <div className='input'>
+                        <img src={email_icon} alt='' />
+                        <input type='email' placeholder='Correo Electrónico' onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='input'>
+                        <img src={password_icon} alt='' />
+                        <input type='password' placeholder='Contraseña' onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                </div>
+                <br></br>
+                <button className='bttn-enviar' type='submit'>
+                    Verificar información
+                </button>
+                {loginError && <div className='error-message'>{loginError}</div>}
+                {loginSuccess && (
+                    <div>
+                        <div className='success-message'>¡Inicio de sesión exitoso, Ya puedes acceder a la tienda!</div>
+                        <button onClick={() => navigate("/")}>Ir a la tienda</button>
+                    </div>
+                )}
+            </form>
+        </div>
+    );
+};
+
+export default Login;
