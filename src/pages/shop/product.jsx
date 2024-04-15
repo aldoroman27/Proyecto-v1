3b8221a5400497ae85c396d8879f4d8805e295e4
aldoroman27 <juggeraldo@gmail.com>
2024-04-15 12:34:16 -0600

    Nuevos cambios

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { ShopContext } from '../../context/shop-context';
+import { useNavigate } from 'react-router-dom';
 
 export const Product = (props) => {
     const {id, productName, price, productImage } = props.data;
     const { addToCart, cartItems } = useContext(ShopContext);
+    const navigate = useNavigate();
 
     const cartItemAmount = cartItems[id];   
   return(
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { PRODUCTS } from '../../products';
 import { Product } from './product';
 import './shop.css';
+import { useNavigate } from 'react-router-dom';
+
 
 export const Shop = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="shop">
       <div className="shopTitle">
@@ -15,6 +19,8 @@ export const Shop = () => {
           <Product data={product}/>
         ))}
       </div>
+          <br></br>
+          <button className='btn-sugest' onClick={() => navigate('/Sugest')}>Recomendaciones</button>
     </div>
   )
 }
